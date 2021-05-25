44a608437f734ad02b2b9182bf8c2d9a4fe94fd2
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 01:50:22 -0700

    adding nav component and basic structure

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,5 +1,30 @@
+import { Link, NavLink, useLocation } from "react-router-dom";
+
 function NavBar() {
-    return <div> Nav Bar </div>
+  const { pathname } = useLocation();
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Google Books Searchinator
+      </Link>
+      <ul className="navbar-nav flex-row mr-auto">
+        <li className="nav-item mx-3">
+          <NavLink className="nav-link" exact to="/">
+            Search{" "}
+            {pathname === "/" && <span className="sr-only">(current)</span>}
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/saved">
+            Saved Books{" "}
+            {pathname === "/saved" && (
+              <span className="sr-only">(current)</span>
+            )}
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
