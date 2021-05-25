65ad6acd22b3ff801db74473b66f34f6ca366f45
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 03:13:16 -0700

    imported section component

--- client/src/components/SearchBox/index.js
+++ client/src/components/SearchBox/index.js
@@ -1,23 +1,22 @@
+import Section from "../Section";
+
 function SearchBox() {
   return (
-    <div className="card bg-dark text-white shadow rounded">
-      <div className="card-body">
-        <h4 className="card-title">Search for a Book!</h4>
-        <form className="d-flex flex-column">
-          <div className="form-group">
-            <label htmlFor="bookSearch">Book</label>
-            <input
-              type="text"
-              className="form-control bg-dark border-secondary text-white shadow rounded"
-              id="bookSearch"
-            />
-          </div>
-          <button className="btn btn-secondary ml-auto shadow rounded px-4">
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    <Section title="Search for a Book!">
+      <form className="d-flex flex-column">
+        <div className="form-group">
+          <label htmlFor="bookSearch">Book</label>
+          <input
+            type="text"
+            className="form-control bg-dark border-secondary text-white shadow rounded"
+            id="bookSearch"
+          />
+        </div>
+        <button className="btn btn-secondary ml-auto shadow rounded px-4">
+          Submit
+        </button>
+      </form>
+    </Section>
   );
 }
 
