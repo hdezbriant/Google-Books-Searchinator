648d6cead195040eca50f9f35b0e53d5e12b5e12
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-24 22:24:40 -0700

    Initialize routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import BookSearchPage from "./components/BookSearch";
+import SavedBooksPage from "./components/SavedBooks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv" alt="logo" />
-      <p>Come back later.</p>
-      </header>
+    <div>
+      <img
+        src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv"
+        alt="logo"
+      />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <BookSearchPage />
+          </Route>
+          <Route path="/saved" >
+            <SavedBooksPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
