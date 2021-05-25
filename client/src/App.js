1653f30e7b5b1b5122b53cfa3085404e7f81f11c
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 04:38:52 -0700

    adding bg styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-import axios from "axios";
+// import axios from "axios";
 import BookSearchPage from "./components/BookSearch";
 import SavedBooksPage from "./components/SavedBooks";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -20,22 +20,18 @@ function App() {
   // });
   // ============== TEST FETCH REQUEST ==============
   return (
-    <div>
-      <img
-        src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv"
-        alt="logo"
-      />
+    <div className="bg-dark">
       <Router>
         <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <BookSearchPage />
-            </Route>
-            <Route path="/saved">
-              <SavedBooksPage />
-            </Route>
-            <Redirect to="/" />
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            <BookSearchPage />
+          </Route>
+          <Route path="/saved">
+            <SavedBooksPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </Router>
     </div>
   );
