f3558cb7adbde738ecb3b2fdbe15fc923a427146
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 02:57:59 -0700

    updating font

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,11 +1,9 @@
 function Hero() {
   return (
-    <div className="jumbotron jumbotron-fluid bg-dark text-white">
+    <div className="jumbotron jumbotron-fluid bg-dark text-white mb-3 shadow rounded">
       <div className="container">
         <h1 className="display-4">Google Books Searchinator</h1>
-        <p className="lead">
-          Search for, and save books you find interesting!
-        </p>
+        <p className="lead font-weight-normal">Search for, and save books you find interesting!</p>
       </div>
     </div>
   );
