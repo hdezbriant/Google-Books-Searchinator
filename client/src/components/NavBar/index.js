cbfc2d4868bb0da54f6e06b2f6fd8bb1e38c4570
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 02:15:56 -0700

    import custom css

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import NavItem from "./NavItem";
+import "./style.css";
 
 function NavBar() {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-dark border-bottom border-warning">
       <Link className="navbar-brand" to="/">
         Google Books Searchinator
       </Link>
