fc3d7bd6feea4ce7281373c11a3a74b2a61767e3
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-24 22:07:46 -0700

    changing in progress image

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv" alt="logo" />
+      <p>Come back later.</p>
       </header>
     </div>
   );
