496cfb2dc2070988bd88f83148f372103009fe2b
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 02:28:08 -0700

    creating hero comp

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,5 +1,14 @@
 function Hero() {
-    return <h1> Hero </h1>
+  return (
+    <div className="jumbotron jumbotron-fluid bg-dark text-white">
+      <div className="container">
+        <h1 className="display-4">Google Books Searchinator</h1>
+        <p className="lead">
+          Search for, and save books you find interesting!
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
