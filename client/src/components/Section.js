68d3993b5efa70245253b2fde0c8ea110ed2308e
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 11:00:29 -0700

    adding margin

--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -1,6 +1,6 @@
 function Section({ title, children }) {
   return (
-    <div className="card bg-dark text-white shadow rounded">
+    <div className="card bg-dark text-white mt-3 shadow rounded">
       <div className="card-body">
         <h4 className="card-title">{title}</h4>
         {children}
