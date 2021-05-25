cdec241a014efa034020904a4a01faf7b93e6b5e
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-24 23:32:09 -0700

    added test for google books API, and importing navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,21 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
+import axios from "axios";
 import BookSearchPage from "./components/BookSearch";
 import SavedBooksPage from "./components/SavedBooks";
+import { useEffect } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
+  // =============== TEST FETCH REQUEST =============
+  // useEffect(() => {
+  //   axios
+  //     .get("https://www.googleapis.com/books/v1/volumes?q=doorsofperception")
+  //     .then(console.log)
+  //     .catch(console.log);
+  // });
+  // ============== TEST FETCH REQUEST ==============
   return (
     <div>
       <img
@@ -15,15 +26,16 @@ function App() {
         alt="logo"
       />
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <BookSearchPage />
-          </Route>
-          <Route path="/saved" >
-            <SavedBooksPage />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
+        <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <BookSearchPage />
+            </Route>
+            <Route path="/saved">
+              <SavedBooksPage />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
       </Router>
     </div>
   );
