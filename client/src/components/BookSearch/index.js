cd82ddb5585c4f95b72e9fdb2a9812556d78f648
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-25 02:29:22 -0700

    add wrapper component

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,14 +1,15 @@
 import Hero from "../Hero";
 import SearchBox from "../SearchBox";
 import BookList from "../BookList";
+import Wrapper from "../Wrapper";
 
 function BookSearchPage() {
   return (
-    <>
+    <Wrapper>
       <Hero />
       <SearchBox />
       <BookList />
-    </>
+    </Wrapper>
   );
 }
 
