33a5a22b18282e8688e464604e8d83fc5543720d
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-26 05:12:02 -0700

    implemented google books api search

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,14 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
 import Hero from "../Hero";
 import SearchForm from "../SearchForm";
 import BookList from "../BookList";
 import Wrapper from "../Wrapper";
 
 function BookSearchPage() {
+  const [search, setSearch] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const handleSearchSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const trimmedSearch = search.trim();
+      if (!trimmedSearch) return;
+
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${trimmedSearch}`;
+      const res = await axios.get(url);
+      setBooks(res.data.items || []);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <Wrapper>
       <Hero />
-      <SearchForm />
-      <BookList />
+      <SearchForm
+        search={search}
+        onSearchChange={(e) => setSearch(e.target.value)}
+        onSubmit={handleSearchSubmit}
+      />
+      <BookList books={books} />
     </Wrapper>
   );
 }
