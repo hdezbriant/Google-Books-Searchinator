a48d5eb8a773d9f924e007d9584206bf1c677c55
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-26 03:54:00 -0700

    Added wrapper component to page view

--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -1,12 +1,13 @@
 import BookList from "../BookList";
 import Hero from "../Hero";
+import Wrapper from "../Wrapper";
 
 function SavedBooksPage() {
   return (
-    <>
+    <Wrapper>
       <Hero />
       <BookList />
-    </>
+    </Wrapper>
   );
 }
 
