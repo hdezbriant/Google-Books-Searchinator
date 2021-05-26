f8b31dac1d1b53019aac9c0597b76c73e21c42da
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-26 05:12:44 -0700

    Added addit'l props for form control

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,15 +1,17 @@
 import Section from "../Section";
 
-function SearchForm() {
+function SearchForm({ search, onSearchChange, onSubmit }) {
   return (
     <Section title="Search for a Book!">
-      <form className="d-flex flex-column">
+      <form className="d-flex flex-column" onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="bookSearch">Book</label>
           <input
             type="text"
             className="form-control bg-dark border-secondary text-white shadow rounded"
             id="bookSearch"
+            value={search}
+            onChange={onSearchChange}
           />
         </div>
         <button className="btn btn-secondary ml-auto shadow rounded px-4">
